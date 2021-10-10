12cb1f106d36df3ec6d4158195517d67d8c51134
fairlee nepunan <fairlee.02@gmail.com>
2021-10-11 00:46:41 +0800

    upload multiple pictures feature

--- ecommerce-back-end/src/models/Product.js
+++ ecommerce-back-end/src/models/Product.js
@@ -16,6 +16,10 @@ const productSchema = new mongoose.Schema({
                 type:Number,
                 required:true
             },
+            quantity: {
+                type:Number,
+                required:true
+            },
             description:{
                 type:String,
                 required: true,
@@ -33,10 +37,13 @@ const productSchema = new mongoose.Schema({
                     review: String
                 }
             ],
-            category:{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' 
+            category:{ 
+                type: mongoose.Schema.Types.ObjectId, ref: 'Category',
+                required:true 
             },
             createdBy: {
-                type: mongoose.Schema.Types.ObjectId, ref: 'User'
+                type: mongoose.Schema.Types.ObjectId, ref: 'User',
+                required: true
             },
             updatedAt:Date,
 }, {timestamps:true});
